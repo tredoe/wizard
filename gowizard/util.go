b95f74b42dd920372df635769f822aad35318b09
John Mac <charles.beck@sent.com>
2010-07-30 16:50:05 +0100

    Added function to read from command line

---
+++ gowizard/util.go
@@ -0,0 +1,39 @@
+// Copyright 2010, The 'gowizard' Authors.  All rights reserved.
+// Use of this source code is governed by the Simplified BSD License
+// that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bufio"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+
+/* Copy a file from the data directory to the project. */
+func copy(destinationFile, sourceFile string) {
+	src, err := ioutil.ReadFile(sourceFile)
+	if err != nil {
+		log.Exit(err)
+	}
+
+	err = ioutil.WriteFile(*fProjectName+destinationFile, src, _PERM_FILE)
+	if err != nil {
+		log.Exit(err)
+	}
+}
+
+/* Reads the standard input until Return is pressed. */
+func read() string {
+	stdin := bufio.NewReader(os.Stdin)
+
+	input, err := stdin.ReadString('\n')
+	if err != nil {
+		log.Exit(err)
+	}
+
+	return input
+}
+
